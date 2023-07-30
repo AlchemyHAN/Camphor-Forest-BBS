161fc9502479adfb83d75e7e90a62475252f7fcc
AlchemyHAN <alchemylinux@gmail.com>
2023-07-30 20:56:58 +0800

    初步添加贴子列表功能

---
+++ components/RecipeReviewCard.js
@@ -0,0 +1,57 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import Avatar from '@mui/material/Avatar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import ShareIcon from '@mui/icons-material/Share';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import moment from "moment";
+
+export default function RecipeReviewCard({passageInfo}) {
+    const date = moment(passageInfo.gmtCreate * 1000);
+    const formattedDate = date.format('YYYY-MM-DD HH:mm');
+
+    return (
+        <Card>
+            <CardHeader
+                avatar={
+                    <Avatar src={passageInfo.avatarUrl}></Avatar>
+                }
+                action={
+                    <IconButton aria-label="settings">
+                        <MoreVertIcon />
+                    </IconButton>
+                }
+                title={passageInfo.name}
+                subheader={formattedDate}
+            />
+            {passageInfo.thumb != null &&
+            <CardMedia
+                component="img"
+                height="194"
+                image={passageInfo.thumb}
+            />}
+            <CardContent>
+                <Typography variant="h5">
+                    {passageInfo.title}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                    {passageInfo.description}
+                </Typography>
+            </CardContent>
+            <CardActions disableSpacing>
+                <IconButton aria-label="add to favorites">
+                    <FavoriteIcon />
+                </IconButton>
+                <IconButton aria-label="share">
+                    <ShareIcon />
+                </IconButton>
+            </CardActions>
+        </Card>
+    );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,55 @@
 import * as React from "react";
 import axios from "axios";
-import {console} from "next/dist/compiled/@edge-runtime/primitives";
 import Navbar from "@/components/navbar";
+import RecipeReviewCard from "@/components/RecipeReviewCard";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
 
-export default function Index({data}){
+export default function Index({navBarData, passageInfoData}) {
 
     return (
-        <Navbar user={data}></Navbar>
+        <div>
+            <Navbar user={navBarData}></Navbar>
+            <Grid container spacing={2}>
+                <Grid item xs={3}>
+
+                </Grid>
+                <Grid item xs={6} sx={{marginBottom: "300px"}}>
+                    {passageInfoData.map((passage, index) => (
+                        <Box my={1} key={index}>
+                            <RecipeReviewCard key={index} passageInfo={passage} />
+                        </Box>
+                    ))}
+                </Grid>
+                <Grid item xs={3}>
+
+                </Grid>
+            </Grid>
+
+        </div>
     );
 }
 
 export async function getServerSideProps(context) {
+    let data;
     try {
         const doorKey = context.req.cookies['doorKey'];
-        console.log(doorKey);
+
         const response = await axios.get("/getNavbarInfo", {
             headers: {
-                "Cookie": "doorKey="+doorKey
-            }});
+                "Cookie": "doorKey=" + doorKey
+            }
+        });
+        data = response.data;
+    } catch (error) {
+        data = null;
+    }
+        const responsePassageInfo = await axios.post("/getPassageInfo");
+
         return {
             props: {
-                data: response.data
+                navBarData: data,
+                passageInfoData: responsePassageInfo.data
             }, // 将响应数据作为属性传递给页面组件
         };
-    } catch (error) {
-        return {
-            props: {
-                data: null,
-            },
-        };
-    }
 }
