5f2f23c29a9883327c40dbb01833cc06b134ecaa
AlchemyHAN <alchemylinux@gmail.com>
2023-08-06 00:19:57 +0800

    初步实现帖子详情内容展示

--- components/PassageCard.js
+++ components/PassageCard.js
@@ -1,7 +1,5 @@
 import * as React from 'react';
 import Card from '@mui/material/Card';
-import CardHeader from '@mui/material/CardHeader';
-import CardMedia from '@mui/material/CardMedia';
 import CardContent from '@mui/material/CardContent';
 import CardActions from '@mui/material/CardActions';
 import Avatar from '@mui/material/Avatar';
@@ -9,15 +7,20 @@ import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import ShareIcon from '@mui/icons-material/Share';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
 import moment from "moment";
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
+import Link from "@mui/material/Link";
+import ReplyIcon from '@mui/icons-material/Reply';
+import Button from "@mui/material/Button";
+import StarBorderIcon from '@mui/icons-material/StarBorder';
+import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
 
-export default function PassageCard({Passage}) {
-    const date = moment(1);
+
+export default function PassageCard({passageData}) {
+    const date = moment(passageData.gmtCreate * 10000);
     const formattedDate = date.format('YYYY-MM-DD HH:mm');
-
     return (
         <Card>
             {/*{passageInfo.thumbnailUrl != null &&*/}
@@ -27,33 +30,62 @@ export default function PassageCard({Passage}) {
             {/*        image={passageInfo.thumbnailUrl}*/}
             {/*    />}*/}
             <CardContent>
-                <Grid container sx={{'--Grid-borderWidth': '3px',
-                    borderTop: 'var(--Grid-borderWidth) solid',
-                    borderLeft: 'var(--Grid-borderWidth) solid',
-                    borderColor: 'red',
-                    '& > div': {
-                        borderRight: 'var(--Grid-borderWidth) solid',
-                        borderBottom: 'var(--Grid-borderWidth) solid',
-                        borderColor: 'red',
-                    },}}>
-                    <Grid item xs={2}>
-                        <Box display="flex"
-                             justifyContent="center">
-                            <Avatar
-                                src="https://picx.zhimg.com/80/v2-1e013030628fe38046c8b08ed15e1e7f_1440w.webp"
-                                sx={{ width: 128, height: 128, textAlign: "center"}}/>
+                <Grid container>
+                    <Grid item xs={12}>
+                        <Box display="flex" justifyContent="space-between"  borderBottom="1px solid #3f51b5">
+                            <Typography variant="h4">
+                                <Box component="span" fontWeight='fontWeightMedium'>{passageData.title}</Box>
+                            </Typography>
+                            <Box>
+                                <Button variant="outlined" sx={{mx: 2}} startIcon={<StarBorderIcon />}>
+                                    收藏
+                                </Button>
+                                <Button variant="outlined" startIcon={<ReplyIcon />}>
+                                    回复
+                                </Button>
+                            </Box>
                         </Box>
                     </Grid>
-                    <Grid item xs={10}><Typography variant="h4"><Box component="span" fontWeight='fontWeightMedium'>啊十九大莱克斯顿</Box></Typography></Grid>
+                    <Grid item xs={2} justifyContent="center">
+                        <Box bgcolor="#f6f7fb">
+                            <Box display="flex" justifyContent="center">
+                                <Avatar
+                                    src={passageData.author.avatarUrl}
+                                    sx={{ width: 128, height: 128, textAlign: "center"}}/>
+                            </Box>
+                            <Box display="flex" justifyContent="center">
+                                <Link href="" align="center" sx={{textDecoration: "none"}}>{passageData.author.name}</Link>
+                            </Box>
+                        </Box>
+                    </Grid>
+                    <Grid item xs={8}>
+                        <Box px={1}>
+                            <ReactMarkdown remarkPlugins={[remarkGfm]} children={passageData.content} />
+                        </Box>
+                    </Grid>
+                    <Grid item xs={2} bgcolor="#f6f7fb">
+                        排行榜功能待做
+                    </Grid>
                 </Grid>
             </CardContent>
-            <CardActions disableSpacing>
-                <IconButton aria-label="add to favorites">
-                    <FavoriteIcon />
-                </IconButton>
-                <IconButton aria-label="share">
-                    <ShareIcon />
-                </IconButton>
+            <CardActions sx={{
+                 display: "flex",
+             }}>
+                <Box justifyContent="space-between" display="flex" flex={1}>
+                    <Box>
+                        <IconButton aria-label="add to favorites">
+                            <FavoriteIcon />
+                        </IconButton>
+                        <IconButton aria-label="share">
+                            <ShareIcon />
+                        </IconButton>
+                    </Box>
+                    <Box display="flex">
+                        <Typography sx={{mx: 1}}>1楼</Typography>
+                        <Typography sx={{mx: 1}}>{formattedDate}</Typography>
+                        <Typography sx={{ml: 1}}>IP属地：重庆</Typography>
+                    </Box>
+                </Box>
             </CardActions>
         </Card>
     );
--- pages/passage/[passageId].js
+++ pages/passage/[passageId].js
@@ -1,24 +1,48 @@
-import { useRouter } from 'next/router';
 import Grid from "@mui/material/Grid";
 import PassageCard from "@/components/PassageCard";
+import axios from "axios";
+import Box from "@mui/material/Box";
 
-function Passage() {
-    const router = useRouter();
-    const { passageId } = router.query;
+export default function Passage({Passage}) {
 
     return (
         <Grid container spacing={2}>
             <Grid item xs={1}>
-                永和九年，岁在癸丑。永和九年，岁在癸丑。永和九年，岁在癸丑。永和九年，岁在癸丑。永和九年，岁在癸丑。永和九年，岁在癸丑。永和九年，岁在癸丑。永和九年，岁在癸丑。永和九年，岁在癸丑。永和九年，岁在癸丑。永和九年，岁在癸丑。
+
             </Grid>
             <Grid item xs={10}>
-                <PassageCard />
+                <Box my={1}>
+                    <PassageCard passageData={Passage} />
+                </Box>
             </Grid>
             <Grid item xs={1}>
-                永和九年，岁在癸丑。永和九年，岁在癸丑。永和九年，岁在癸丑。永和九年，岁在癸丑。永和九年，岁在癸丑。永和九年，岁在癸丑。永和九年，岁在癸丑。永和九年，岁在癸丑。
+
             </Grid>
         </Grid>
     );
 }
+export async function getServerSideProps(context) {
+    const passageId  = context.query.passageId;
+    console.log(passageId);
+    const doorKey = context.req.cookies['doorKey'];
+    if (doorKey == null) {
+        return {
+            redirect: {
+                permanent: false,
+                destination: '/login',
+            },
+        };
+    }
 
-export default Passage;
+    const response = await axios.get("/passage/" + passageId, {
+        headers: {
+            "Cookie": "doorKey=" + doorKey
+        }
+    });
+    return {
+        props: {
+            Passage: response.data
+        }, // 将响应数据作为属性传递给页面组件
+    };
+}
+
